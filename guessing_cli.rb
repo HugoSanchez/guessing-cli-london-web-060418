81d7e44466a5a77803f248fd13027e80ebdd7edc
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-30 10:51:54 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
       return "Goodbye!"
     elsif user_input == number
       puts "You guessed the correct number!"
-    else
-      puts "The computer guessed #{number}."
+      else
+        puts "The computer guessed #{number}."
   end
 end
